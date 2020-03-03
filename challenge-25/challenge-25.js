9769420aa5c0e18d0f32dd43467362d71fbac4b7
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-03-03 14:52:45 -0300

    challenge 25 started

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,15 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+const log = document.querySelector('.event-log-contents');
+
+const badImg = document.querySelector('.bad-img');
+badImg.addEventListener('error', (event) => {
+    log.textContent = `Deu certo? ${event.type}: Loading image\n`;
+    console.log(event)
+});
+
+const imgError = document.querySelector('#img-error');
+imgError.addEventListener('click', () => {
+    badImg.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg');
+});
